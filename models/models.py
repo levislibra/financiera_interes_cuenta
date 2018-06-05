b4db785e8397c4d89877a51cbba3f590dceecde7
Levis <levislibra@hotmail.com>
2018-06-04 23:12:10 -0300

    Update

--- models/models.py
+++ models/models.py
@@ -45,6 +45,63 @@ class FinancieraDescubierto(models.Model):
 		return rec
 
 	@api.one
+	def create_saldo_mes(self):
+		cr = self.env.cr
+		uid = self.env.uid
+		partner_obj = self.pool.get('res.partner')
+		partner_ids = partner_obj.search(cr, uid, [
+			('customer', '=', True),
+			('compute_fin_mes', '=', True),
+			])
+		for _id in partner_ids:
+			partner_id = self.env['res.partner'].browse(_id)
+			print partner_id.name
+			if partner_id.date_first_move != False:
+				date_first_move = datetime.strptime(partner_id.date_first_move, "%Y-%m-%d")
+				date_finish_month = date_month = datetime(date_first_move.year, date_first_move.month, calendar.monthrange(date_first_move.year, date_first_move.month)[1])
+				current_date = datetime.now()
+				while date_finish_month <= current_date:
+					move_line_obj = self.pool.get('account.move.line')
+					move_line_ids = move_line_obj.search(cr, uid, [
+						('partner_id', '=', partner_id.id),
+						('account_id', '=', partner_id.property_account_receivable_id.id),
+						('date', '=', date_finish_month)
+					])
+					if len(move_line_ids) == 0:
+						# No hay movimiento en fin de mes => crearlo
+						aml = {
+						    'name': "Saldo fin de mes",
+						    'account_id': partner_id.property_account_receivable_id.id,
+						    'journal_id': partner_id.journal_fin_de_mes.id,
+						    'date': date_finish_month,
+						    'date_maturity': date_finish_month,
+						    'partner_id': partner_id.id,
+						}
+
+						aml2 = {
+						    'name': "Saldo fin de mes",
+						    'account_id': partner_id.journal_fin_de_mes.default_debit_account_id.id,
+						    'journal_id': partner_id.journal_fin_de_mes.id,
+						    'date': date_finish_month,
+						    'date_maturity': date_finish_month,
+						    'partner_id': partner_id.id,
+						}
+						am_values = {
+						    'journal_id': partner_id.journal_fin_de_mes.id,
+						    'partner_id': partner_id.id,
+						    'state': 'draft',
+						    'date': date_finish_month,
+						    'line_ids': [(0, 0, aml), (0, 0, aml2)],
+						}
+						new_move_id = self.env['account.move'].create(am_values)
+						new_move_id.post()
+					if date_finish_month.month == 12:
+						date_finish_month = datetime(date_finish_month.year+1, 1, 31)
+					else:
+						date_finish_month = datetime(date_finish_month.year, date_finish_month.month+1, calendar.monthrange(date_finish_month.year, date_finish_month.month+1)[1])
+
+
+	@api.one
 	def _compute_is_credit(self):
 		self.is_credit = self.partner_id.property_account_receivable_id.user_type_id.type == 'receivable'
 
@@ -52,22 +109,24 @@ class FinancieraDescubierto(models.Model):
 	def _compute_matrix(self):
 		print "_compute_matrixxxxxxxxxxxxxxxxxxxxx"
 		global array_lines
-		if self.date_start:
-			date_start = datetime.strptime(self.date_start, "%Y-%m-%d")
+		#if self.date_start:
+		date_start = datetime.strptime(self.line_ids[len(self.line_ids)-1].date, "%Y-%m-%d")
 		if self.date_finish:
 			date_finish = datetime.strptime(self.date_finish, "%Y-%m-%d")
 		result = date_finish - date_start
-		array_lines = [[date_start + timedelta(days=x), None, 0.0] for x in range(0, result.days+1)]
+		len_matrix = result.days
+		array_lines = [[date_start + timedelta(days=x), None, 0.0] for x in range(0, len_matrix+1)]
 		old_days = -1
+		print "arrays len:"
+		print len(array_lines)
 		for line_id in self.line_ids:
 			current_date = datetime.strptime(line_id.date, "%Y-%m-%d")
 			result = current_date - date_start
+			print "position"
 			print result.days
-			if result.days != old_days:
+			if result.days != old_days and result.days < len_matrix:
 				array_lines[result.days][1] = line_id.total_balance_receivable
 			old_days = result.days
-		print result.days
-		print array_lines
 
 	@api.one
 	def _compute_interes(self):
@@ -75,38 +134,35 @@ class FinancieraDescubierto(models.Model):
 		global array_lines
 		date_start = datetime.strptime(self.date_start, "%Y-%m-%d")
 		date_finish = datetime.strptime(self.date_finish, "%Y-%m-%d")
-		i = 0
+		init = date_start - datetime.strptime(self.line_ids[len(self.line_ids)-1].date, "%Y-%m-%d")
+		print "Position init::"
+		print init.days
+		i = init.days
 		if 'array_lines' in globals():
 			len_array_lines = len(array_lines)
 			interes_acumulado_mensual = 0
-			# print array_lines
 			while i < len_array_lines:
 				line = array_lines[i]
 				current_date = line[0]
-				# print "Current date"
-				# print current_date			
-				# saldo = line[1]+line[2]
+				if i == init.days:
+					print "Fecha inicial checada es::"
+					print current_date
 				fin_de_mes = datetime(current_date.year, current_date.month, calendar.monthrange(current_date.year, current_date.month)[1])
 				if current_date == fin_de_mes or current_date == date_finish:
 					if interes_acumulado_mensual > 0:
-						# Hay que facturar::::
 						self.generate_invoice(current_date, interes_acumulado_mensual)
 						interes_acumulado_mensual = 0
-						print "facturamos"
 				if line[1] == None and i > 0:
 					line[1] = array_lines[i-1][1]
-					print "asigamos saldo anterior"
 				if i > 0:
-					#print "Capitalizamos y unificamos saldo"
 					line[2] += array_lines[i-1][2]
-				#line[1] += line[2]
-				saldo = line[1] + line[2]
-				print line
+				if line[1] != None:
+					saldo = line[1] + line[2]
+				else:
+					saldo = 0
 				if saldo > 0:
 					# Esta debiendo dinero en cuenta
 					interes = round(saldo * self.rate_per_day, 2)
-					print "interes::"
-					print interes
 					interes_acumulado_mensual += interes
 					if self.capitalization == 'diaria':
 						if current_date < date_finish:
@@ -121,31 +177,19 @@ class FinancieraDescubierto(models.Model):
 						position = 0
 						if current_date < date_biweekly_capitalizacion:
 							if date_biweekly_capitalizacion <= date_finish:
-								# print "es quincenal "
-								# print date_biweekly_capitalizacion
 								position = date_biweekly_capitalizacion - date_start
 							else:
-								# print "es date_finish quincenal"
-								# print date_finish
 								position = date_finish - date_start
 						elif current_date < date_month_capitalizacion:
 							if date_month_capitalizacion <= date_finish:
-								# print "es date_month_capitalizacion"
-								# print date_month_capitalizacion
 								position = date_month_capitalizacion - date_start
 							else:
-								# print "es date_finish mensual"
-								# print date_finish
 								position = date_finish - date_start
 						else:
 							# La fecha evaluada es fin de mes
 							if date_biweekly_next_month_capitalizacion <= date_finish:
-								# print "es date_biweekly_next_month_capitalizacion"
-								# print date_biweekly_next_month_capitalizacion
 								position = date_biweekly_next_month_capitalizacion - date_start
 							else:
-								# print "es date_finish next biweekly month"
-								# print date_finish
 								position = date_finish - date_start
 						array_lines[position.days][2] += interes
 					elif self.capitalization == 'mensual':
@@ -154,34 +198,22 @@ class FinancieraDescubierto(models.Model):
 							date_next_month_capitalizacion = datetime(current_date.year, current_date.month+1, calendar.monthrange(current_date.year, current_date.month+1)[1])
 						elif current_date.month == 12:
 							date_next_month_capitalizacion = datetime(current_date.year+1, 1, calendar.monthrange(current_date.year+1, 1)[1])
-						# print "posibles fechas"
-						# print date_month_capitalizacion
-						# print date_next_month_capitalizacion
-						# print date_finish
 						position = 0
 						if current_date < date_month_capitalizacion:
 							if date_month_capitalizacion <= date_finish:
-								# print "es date_month_capitalizacion"
-								# print date_month_capitalizacion
 								position = date_month_capitalizacion - date_start
 							else:
-								# print "es date_finish mensual"
-								# print date_finish
 								position = date_finish - date_start
 						else:
 							# La fecha evaluada es fin de mes
 							if date_next_month_capitalizacion <= date_finish:
-								# print "es date_biweekly_next_month_capitalizacion"
-								# print date_next_month_capitalizacion
 								position = date_next_month_capitalizacion - date_start
 							else:
-								# print "es date_finish month"
-								# print date_finish
 								position = date_finish - date_start
-						# print "position::"
-						# print position.days
 						array_lines[position.days][2] += interes
 				i += 1
+			self.state = 'facturado'
+			self.partner_id.last_date_compute_interes = self.date_finish
 		else:
 			raise ValidationError('Envie a borrador y vuelva a computar.')
 
@@ -231,41 +263,49 @@ class FinancieraDescubierto(models.Model):
 					new_invoice_id.signal_workflow('invoice_open')
 			self.invoice_ids = [new_invoice_id.id]
 
+	@api.one
+	def set_date_start(self):
+		if self.partner_id.last_date_compute_interes == False:
+			if len(self.line_ids) > 0:
+				self.date_start = self.line_ids[len(self.line_ids)-1].date
+		else:
+			self.date_start = self.partner_id.last_date_compute_interes
 
 	@api.one
 	def set_lines(self):
-		print "set_linessssssssssssssssssssssssssss"
 		self.line_ids = None
 		cr = self.env.cr
 		uid = self.env.uid
 		move_line_obj = self.pool.get('account.move.line')
-		if self.partner_id.last_date_compute_interes == False:
-			move_line_ids = move_line_obj.search(cr, uid, [
-				('partner_id', '=', self.partner_id.id),
-				('account_id', '=', self.account_id.id),
-				])
-		else:
-			move_line_ids = move_line_obj.search(cr, uid, [
-				('partner_id', '=', self.partner_id.id),
-				('account_id', '=', self.account_id.id),
-				('date', '>=', self.partner_id.last_date_compute_interes),
-				])
+		move_line_ids = move_line_obj.search(cr, uid, [
+			('partner_id', '=', self.partner_id.id),
+			('account_id', '=', self.account_id.id),
+		])
 		self.line_ids = move_line_ids
-		if len(self.line_ids) > 0:
-			self.date_start = self.line_ids[len(self.line_ids)-1].date
+		
 
 	@api.one
 	def procesar(self):
-		print "procesarrrrrrrrrrrrrrrrrrrrrrrrr"
 		self.set_lines()
+		self.set_date_start()
 		self._compute_matrix()
-		#self._compute_interes()
 		self.state = 'procesando'
 
 	@api.one
 	def draft(self):
-		print "drafttttttttttttttttttttttttttttttttt"
 		self.state = 'borrador'
+
+	@api.one
+	def cancelar_descubierto(self):
+		for invoice_id in self.invoice_ids:
+			if invoice_id.state == 'open':
+				invoice_id.signal_workflow('invoice_cancel')
+			elif invoice_id.state == 'paid':
+				# Crear factura rectificativa
+				# O romper conciliacion
+				pass
+		self.state = 'cancelado'
+		self.partner_id.last_date_compute_interes = self.date_start
 
 	@api.multi
 	def wizard_descubierto_facturar(self):
@@ -290,6 +330,24 @@ class FinancieraDescubierto(models.Model):
 			'target': 'new',
 		}
 
+	@api.multi
+	def wizard_cancelar(self):
+		params = {
+			'descubierto_id': self.id,
+		}
+		view_id = self.env['financiera.descubierto.wizard']
+		new = view_id.create(params)
+		return {
+			'type': 'ir.actions.act_window',
+			'name': 'Cancelar Descubierto y Facturas',
+			'res_model': 'financiera.descubierto.wizard',
+			'view_type': 'form',
+			'view_mode': 'form',
+			'res_id'    : new.id,
+			'view_id': self.env.ref('financiera_interes_cuenta.cancelar_descubierto_wizard', False).id,
+			'target': 'new',
+		}
+
 
 class FinancieraDescubiertoConfig(models.Model):
 	_name = 'financiera.descubierto.config'
@@ -308,7 +366,70 @@ class ExtendsAccountMoveLine(models.Model):
 	_inherit = 'account.move.line'
 
 	descubierto_id = fields.Many2one('financiera.descubierto', 'Calculo de descubierto')
+	interes_no_consolidado_amount = fields.Monetary("Interes no consolidado", compute='_compute_interes_no_consolidado')
+	interes_no_consolidado_acumulado = fields.Monetary("Interes no consolidado acumulado")	
 	interes_computado = fields.Boolean('Descubierto computado', default=False)
+	dias = fields.Float('Dias')
+	balance_anterior = fields.Float('Balance')
+
+	@api.multi
+	def _compute_interes_no_consolidado(self):
+		print "_compute_interes_no_consolidado**-*--*-*-"
+		print self
+		if len(self) > 0:
+			line_base_id = self.env['account.move.line'].browse(self[0].id)
+			print line_base_id
+			cr = self.env.cr
+			uid = self.env.uid
+			move_line_obj = self.pool.get('account.move.line')
+			move_line_ids = move_line_obj.search(cr, uid, [
+				('partner_id', '=', line_base_id.partner_id.id),
+				('account_id', '=', line_base_id.account_id.id),
+			])
+			print "lista"
+			print move_line_ids
+			prev_line_id = None
+			balance = 0
+			i = len(move_line_ids)-1
+			print "Comienza while, valor de i es::"
+			print i
+			while i >= 0:
+				line_id = self.env['account.move.line'].browse(move_line_ids[i])
+				# Saldo deudor del cliente
+				date_finish = datetime.strptime(line_id.date, "%Y-%m-%d")
+				print "Objeto observado:"
+				print i
+				print date_finish
+				interes_no_consolidado_previo = 0
+				if i < len(move_line_ids)-1:
+					prev_line_id = self.env['account.move.line'].browse(move_line_ids[i+1])
+					date_start = datetime.strptime(prev_line_id.date, "%Y-%m-%d")
+					interes_no_consolidado_previo = prev_line_id.interes_no_consolidado_amount
+					balance = prev_line_id.total_balance_receivable + interes_no_consolidado_previo
+					prev_interes_no_consolidado_acumulado = prev_line_id.interes_no_consolidado_acumulado
+					print "Desde::"
+					print date_start
+				else:
+					print "entro en else"
+					date_start = date_finish
+					balance = 0
+					prev_interes_no_consolidado_acumulado = 0
+					pass
+
+				dias = date_finish - date_start
+				dias = dias.days
+				line_id.balance_anterior = balance
+				print 'balance anterior::'
+				print balance
+				line_id.dias = dias
+				print 'dias::'
+				print dias
+				line_id.interes_no_consolidado_acumulado = line_id.interes_no_consolidado_amount + prev_interes_no_consolidado_acumulado
+				if balance > 0:
+					line_id.interes_no_consolidado_amount = dias * balance * line_id.partner_id.rate_per_day
+				i -= 1
+
+
 
 class ExtendsAccountMove(models.Model):
 	_name = 'account.move'
@@ -327,3 +448,24 @@ class ExtendsAccountResPartner(models.Model):
 	_inherit = 'res.partner'
 
 	last_date_compute_interes = fields.Date('Ultima fecha de interes computado', default=False)
+	move_line_ids = fields.One2many('account.move.line', 'partner_id', 'Movimientos')
+	# move_ids = fields.One2many('account.move', '' 'Asientos fin de mes')
+	date_first_move = fields.Date('Primer movimiento', compute='_compute_date_first_move')
+	compute_fin_mes = fields.Boolean('Movimientos de fin de mes', default=False)
+	journal_fin_de_mes = fields.Many2one('account.journal', 'Diario de saldos')
+	capitalization = fields.Selection([('diaria', 'Diaria'), ('mensual', 'Mensual')], string='Capitalizacion', required=True, default='mensual')
+	rate_per_day = fields.Float('Tasa diaria', digits=(16,6))
+
+	@api.one
+	def _compute_date_first_move(self):
+		if len(self.move_line_ids) > 0:
+			self.date_first_move = self.move_line_ids[len(self.move_line_ids)-1].date
+		else:
+			self.date_first_move = False
+
+	@api.one
+	@api.constrains('journal_fin_de_mes')
+	def _check_journal_fin_mes(self):
+		if self.compute_fin_mes:
+			if len(self.journal_fin_de_mes.default_debit_account_id) == 0:
+				raise ValidationError("Debe definir una cuenta en el diario de saldos.")
--- wizards/financiera_descubierto_wizard.py
+++ wizards/financiera_descubierto_wizard.py
@@ -19,3 +19,8 @@ class FinancieraDescubiertoWizard(models.TransientModel):
         descubierto_id.journal_id = self.journal_id
         descubierto_id._compute_interes()
         descubierto_id.set_lines()
+
+    @api.one
+    def cancelar_descubierto(self):
+        descubierto_id = self.descubierto_id
+        descubierto_id.cancelar_descubierto()
